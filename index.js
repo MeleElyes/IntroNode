027ae827ee4902b497f36b58133e7f46c58687be
Elyes MELE <mele.elyes@gmail.com>
2023-01-20 11:50:17 +0100

    intro

---
+++ index.js
@@ -0,0 +1,5 @@
+const chalk = require("chalk");
+    console.log(chalk.green("Elyes"));
+    console.log(chalk.red("Paul"));
+    console.log(chalk.blue("Marion"));
+    console.log(chalk.yellow("Momo"));
